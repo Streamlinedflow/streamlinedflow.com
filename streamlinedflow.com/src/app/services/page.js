07fcbd905cbe1c7a2d0450377217e4b7beea9ae3
Camron Walsh <camronwalsh@gmail.com>
2025-05-17 09:42:27 -0400

    Refactored services layout

--- streamlinedflow.com/src/app/services/page.js
+++ streamlinedflow.com/src/app/services/page.js
@@ -9,18 +9,35 @@ import { useTheme } from "@/context/ThemeContext";
 import Footer from "@/components/layout/Footer";
 import Navbar from "@/components/layout/Navbar";
 import CustomAgentService from "@/features/services/CustomAgent";
-import ServicesCTA from "@/features/services/ServiceCTA";
+import NextStepsCTA from "@/components/common/NextStepsCTA";
 import ChatbotDevelopmentService from "@/features/services/CustomChatbot";
+import Container from "@/components/layout/Container";
+import Section from "@/components/layout/Section";
 
 export default function ServicesPage() {
   return (
     <div className="min-h-screen bg-gray-100 text-gray-950 dark:bg-zinc-950 dark:text-gray-50">
       <Navbar />
       <main className="min-h-screen">
-        <CustomAgentService />
-        <ChatbotDevelopmentService />
-        <ServicesCTA />
-        <Footer />
+        <Container>
+
+          <Section>
+            <CustomAgentService />
+          </Section>
+
+          <Section>
+            <ChatbotDevelopmentService />
+          </Section>
+
+          <Section>
+            <NextStepsCTA />
+          </Section>
+
+          <Section>
+            <Footer />
+          </Section>
+
+        </Container>
       </main>
     </div>
   );
