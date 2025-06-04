c0b12ff2f98b510950de7375929a2257f94f4209
Streamlinedflow <liam@streamlinedflow.com>
2025-06-04 11:37:41 -0400

    Update Services.js

--- streamlinedflow.com/src/features/landing/Services.js
+++ streamlinedflow.com/src/features/landing/Services.js
@@ -18,11 +18,11 @@ export default function Services() {
           <Link href="/services?service=autonomous-agent" className="block">
             <div className="rounded-xl backdrop-blur-lg border-2 border-transparent bg-white dark:bg-zinc-900 p-6 flex flex-col h-full cursor-pointer transition-transform duration-400 transform hover:scale-105 hover:border-blue-400 hover:shadow-lg hover:shadow-blue-400/20">
               <h3 className="text-xl font-bold mb-4 text-gray-900 dark:text-white inline-block">
-                Custom AI Automation Systems
+                AI Infrastructures
                 <span className="block h-1 w-12 bg-gradient-to-r from-sky-500 to-blue-600 rounded-full" />
               </h3>
               <p className="text-gray-700 dark:text-gray-300 leading-relaxed text-sm md:text-base mb-6">
-                Scalable, affordable systems to grow your company on autopilot.
+                AI systems to streamline and grow your company.
               </p>
               <span className="text-blue-500 hover:underline mt-auto">Read more</span>
             </div>
@@ -31,11 +31,11 @@ export default function Services() {
           <Link href="/services?service=chatbot" className="block">
             <div className="rounded-xl backdrop-blur-lg border-2 border-transparent bg-white dark:bg-zinc-900 p-6 flex flex-col h-full cursor-pointer transition-transform duration-400 transform hover:scale-105 hover:border-blue-400 hover:shadow-lg hover:shadow-blue-400/20">
               <h3 className="text-xl font-bold mb-4 text-gray-900 dark:text-white inline-block">
-                Custom Chatbot Development
+                Voice AI
                 <span className="block h-1 w-12 bg-gradient-to-r from-sky-500 to-blue-600 mt-2 rounded-full" />
               </h3>
               <p className="text-gray-700 dark:text-gray-300 leading-relaxed text-sm md:text-base mb-6">
-                Enhance user engagement and streamline interactions with our custom chatbot development services.
+                Enhance user engagement and streamline interactions with our voice AI systems.
               </p>
               <span className="text-blue-500 hover:underline mt-auto">Read more</span>
             </div>
